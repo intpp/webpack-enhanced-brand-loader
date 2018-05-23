331abaa9b8f80eab24a16b0810a09d91e3a6bb29
Valery Zakharchenko <valprogramm@gmail.com>
2018-05-23 19:35:36 +0300

    Added support for .html files

--- loader.js
+++ loader.js
@@ -9,47 +9,57 @@ const regex = /\.[^.]+$/;
 
 let notified = false;
 
-module.exports = function (content, map, meta) {
+const load = (originalResource, brand, callback) => {
+  const extension = originalResource.match(regex)[0];
+  const isHtml = extension === '.html';
+  const encoding = isHtml ? 'utf-8' : null;
+
+  const resource = brand ? originalResource.replace(regex, `.${brand}${extension}`) : originalResource;
+  const stats = fs.statSync(resource);
+
+  const content = fs.readFileSync(resource, encoding);
+
+  return {
+    resource,
+    content,
+    isHtml,
+  };
+};
+
+module.exports = function (originalContent, map, meta) {
   const options = getOptions(this);
+  const { brand } = options;
 
   validateOptions(schema, options, packageName);
 
-  const { brand } = options;
-
   const callback = this.async();
-  let { resource, addDependency } = this;
+  const { resource: originalResource, addDependency } = this;
 
-  if (brand) {
-    if (!notified) {
+  if (!notified) {
+    notified = true;
+
+    if (brand) {
       console.log(chalk.green(`${packageName}: brand = ${brand}`));
-      notified = true;
-    }
-
-    try {
-      const extension = resource.match(regex)[0];
-      const nextFilePath = resource.replace(regex, `.${brand}${extension}`);
-
-      const stats = fs.statSync(nextFilePath);
-
-      if (stats.isFile()) {
-        return fs.readFile(nextFilePath, null, function (err, data) {
-          if (err) {
-            return callback(err);
-          }
-
-          addDependency(nextFilePath);
-          callback(null, data, map, meta);
-        });
-      }
-    } catch (e) {
-    }
-  } else {
-    if (!notified) {
+    } else {
       console.log(chalk.yellow(`${packageName}: Option "brand" has not been set, using default (no brand).`));
-      notified = true;
     }
   }
 
-  callback(null, content, map, meta);
+  try {
+    const { resource, content, isHtml } = load(originalResource, brand);
+
+    if (originalResource !== resource) {
+      addDependency(resource);
+    }
+
+    if (isHtml) {
+      return callback(null, `module.exports = ${JSON.stringify(content)}`, map, meta);
+    }
+
+    return callback(null, content, map, meta);
+  } catch (e) {
+    return callback(null, originalContent, map, meta);
+  }
 };
+
 module.exports.raw = true;
